92a1a5e8a7de1773c64d41094585ce954aa1ef63
Milan Miletic <milan.miletic@rute.email>
2026-06-29 23:54:56 +0200

    Fixed CORS error, removed db port from docker-compose

--- FlightScan-be/FlightScan.Api/Program.cs
+++ FlightScan-be/FlightScan.Api/Program.cs
@@ -27,7 +27,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials());
